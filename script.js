7e754b0f2d353b62e50a171cacac2cc90353278f
Luiscarreras88 <lc.carreras88@gmail.com>
2023-05-18 23:15:25 -0300

    galeria de imagenes

--- script.js
+++ script.js
@@ -2,3 +2,45 @@ document.querySelector("button.button-menu-toggle")
     .addEventListener("click", function() {
         document.querySelector(".nav-links").
         classList.toggle("nav-links-responsive")})
+
+//carrusel
+var images = [
+        "imagenes/yo1.jpg",
+        "imagenes/yo2.jpg",
+        "imagenes/yo3.jpg",
+        "imagenes/yo4.jpg"
+];
+
+// Obtiene la referencia a los elementos del carrusel
+var carouselImage = document.getElementById("carousel-image");
+var prevButton = document.getElementById("prev-button");
+var nextButton = document.getElementById("next-button");
+
+// Configura un índice inicial
+var currentIndex = 0;
+
+// Función para cambiar la imagen del carrusel
+function changeImage() {
+        // Actualiza la ruta de la imagen
+        carouselImage.src = images[currentIndex];
+}
+// Función para ir a la imagen anterior
+function goToPreviousImage() {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        changeImage();
+}
+
+// Función para ir a la imagen siguiente
+function goToNextImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        changeImage();
+}
+
+// Asigna los manejadores de eventos a los botones
+prevButton.addEventListener("click", goToPreviousImage);
+nextButton.addEventListener("click", goToNextImage);
+
+// Inicia el carrusel con la primera imagen
+changeImage();
+
+//fin carrusel
